9ada619be1566df007d0eb1a9465ca79d98d28cc
Ray Wojciechowski <ray.wojciechowski@ars.de>
2025-12-06 11:14:15 +0100

    Add contrast route and start component

---
+++ src/app/contrast/components/contrast/contrast.ts
@@ -0,0 +1,12 @@
+import {Component} from '@angular/core';
+
+
+@Component({
+  selector: 'ct-contrast',
+  imports: [],
+  templateUrl: './contrast.html',
+  styles: ``,
+})
+export class Contrast {
+
+}
