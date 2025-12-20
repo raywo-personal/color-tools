9c3408420cfa4be4e1b36d40cee1143a32b88c29
Ray Wojciechowski <ray.wojciechowski@ars.de>
2025-12-20 12:39:51 +0100

    Update `webFontsApiUrl` path in environment config
    Change the `webFontsApiUrl` in the gh-pages environment to include the
    `/webfonts` path for accurate API endpoint usage.

--- src/environments/environment.gh-pages.ts
+++ src/environments/environment.gh-pages.ts
@@ -3,5 +3,5 @@ import {Environment} from "./environment.model";
 
 export const environment: Environment = {
   quotesApiUrl: "https://quote-proxy.ray-7b9.workers.dev/api",
-  webFontsApiUrl: "https://google-webfonts-proxy.ray-7b9.workers.dev"
+  webFontsApiUrl: "https://google-webfonts-proxy.ray-7b9.workers.dev/webfonts"
 };
